506f68dba7a499e041b7e86961187e0d5a902e38
icealtria <icealtria@gmail.com>
2022-11-01 17:17:25 +0800

    lab 6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -5,13 +5,12 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author icealtria
 */
-public class Dog { // TODO
+public class Dog implements java.io.Serializable{
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = join(CapersRepository.CAPERS_FOLDER, "dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +38,7 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        return readObject(join(DOG_FOLDER, name), Dog.class);
     }
 
     /**
@@ -48,7 +46,7 @@ public class Dog { // TODO
      */
     public void haveBirthday() {
         age += 1;
-        System.out.println(toString());
+        System.out.println(this.toString());
         System.out.println("Happy birthday! Woof! Woof!");
     }
 
@@ -56,7 +54,7 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        writeObject(join(DOG_FOLDER, this.name), this);
     }
 
     @Override
